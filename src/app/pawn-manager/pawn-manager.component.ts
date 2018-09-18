2f5e42e34ae5d1695171b23491be0e357a575517
Maxim <mma29121983@gmail.com>
2018-09-18 19:03:43 -0400

    fixes lint warnings (#4)

--- src/app/pawn-manager/pawn-manager.component.ts
+++ src/app/pawn-manager/pawn-manager.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy, OnChanges, SimpleChanges } from '@angular/core';
-import { Pawn } from '../data-structures/pawn/pawn';
+import {
+  Component,
+  OnInit,
+  Input,
+  ChangeDetectionStrategy,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+import {Pawn} from '../data-structures/pawn/pawn';
 
 @Component({
   selector: 'web-checkers-pawn-manager',
@@ -22,7 +29,7 @@ export class PawnManagerComponent implements OnInit, OnChanges {
   private adjustPawnPosition(pawn: Pawn): Pawn {
     const col = pawn.currentCol;
     const row = pawn.currentRow;
-    const left = parseFloat(this.size)* col;
+    const left = parseFloat(this.size) * col;
     const top = parseFloat(this.size) * row;
     pawn.left = `${left}px`;
     pawn.top = `${top}px`;
@@ -39,9 +46,9 @@ export class PawnManagerComponent implements OnInit, OnChanges {
 
   private generateDefaultState(startRow: number, color: string): Pawn[] {
     const pawns: Pawn[] = [];
-    for(let i = 0; i < 12; i++) {
-      const row = ((i / 4) | 0) + startRow;
-      const column = ((i%4) * 2 + 1 - row % 2);
+    for (let i = 0; i < 12; i++) {
+      const row = (Math.floor(i / 4)) + startRow;
+      const column = (i % 4) * 2 + 1 - (row % 2);
       pawns.push(this.createPawn(color, row, column));
     }
 
@@ -51,8 +58,8 @@ export class PawnManagerComponent implements OnInit, OnChanges {
   ngOnInit() {
     this.pawns = [
       ...this.generateDefaultState(0, 'black'),
-      ...this.generateDefaultState(5, 'white')
-    ]
+      ...this.generateDefaultState(5, 'white'),
+    ];
   }
 
   ngOnChanges(simpleChanges: SimpleChanges) {
